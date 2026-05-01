ed6244ea3e218f598da7631b4d4f8f2ca47af39e
kaustubh285 <kaustubh285@gmail.com>
2026-05-01 22:42:08 +0100

    fix redirect

--- packages/orbit-frontend/src/App.tsx
+++ packages/orbit-frontend/src/App.tsx
@@ -1,27 +1,24 @@
 import { AppShell, Center, Loader } from "@mantine/core"
-import { Outlet, useNavigate } from "@tanstack/react-router"
+import { Outlet } from "@tanstack/react-router"
 import { useAuth } from "@clerk/react"
 import "./app.css"
 import { AppHeader } from "./components/app-header.component"
 import { AppFooter } from "./components/app-footer.component"
 import { CreateNewComponent } from "./components/create-new.component"
-import ROUTES from "./routes"
 
 export function App() {
 	const { isLoaded, isSignedIn } = useAuth()
-	const navigate = useNavigate()
 
-	if (!isSignedIn) {
-		navigate({
-			to: ROUTES.LOGIN
-		})
-	}
 	if (!isLoaded) {
 		return (
 			<Center h="100dvh">
 				<Loader />
 			</Center>
 		)
+	}
+
+	if (!isSignedIn) {
+		return <Outlet />
 	}
 
 	return (
