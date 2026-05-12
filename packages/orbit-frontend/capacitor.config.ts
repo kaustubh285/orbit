81873c4ac16718cf696316afc04c5a9734895c12
kaustubh285 <kaustubh285@gmail.com>
2026-05-12 21:49:42 +0100

    app init

---
+++ packages/orbit-frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'page.deshpande.orbit',
+  appName: 'orbit',
+  webDir: 'dist'
+};
+
+export default config;
